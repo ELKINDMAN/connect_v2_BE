909c31a6a614e8300a8c639fc761aaa8134edcae
Kindness Mbaekwe <elkanahkindness@gmail.com>
2025-06-25 19:44:28 +0100

    Update extensions.py flask_cors

--- app/extensions.py
+++ app/extensions.py
@@ -1,8 +1,10 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_mail import Mail
 from flask_migrate import Migrate
+from flask_cors import CORS
 
 
 db = SQLAlchemy()
 migrate = Migrate()
 mail = Mail()
+cors = CORS()
